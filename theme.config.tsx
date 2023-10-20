392d3f73143778cd1366cb1f665a32fdb3345831
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-10-21 00:57:20 +0530

    wtf

---
+++ theme.config.tsx
@@ -0,0 +1,56 @@
+import type { DocsThemeConfig } from "nextra-theme-docs";
+import { useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import React from "react";
+
+const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s | RayAuth",
+      };
+    }
+    return {};
+  },
+
+  logo: (
+    <>
+      <span
+        style={{
+          marginLeft: ".4em",
+          marginTop: ".1em",
+          fontWeight: 600,
+          fontSize: "1.2em",
+        }}
+      >
+        Pearlx Docs
+      </span>
+    </>
+  ),
+  docsRepositoryBase:
+    "https://github.com/AnishDe12020/rayauth/tree/main/apps/docs",
+  project: {
+    link: "https://github.com/AnishDe12020/rayauth",
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true,
+  },
+  footer: {
+    text: (
+      <span>
+        {new Date().getFullYear()} ©{" "}
+        <a href="https://rayauth.com" target="_blank" rel="noreferrer">
+          RayAuth
+        </a>
+        .
+      </span>
+    ),
+  },
+  feedback: {
+    content: "Feedback? Submit an issue",
+  },
+};
+
+export default config;
